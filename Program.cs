69394f9d3d914b2dce93d7a34dfee8685d538d52
Folasayo Samuel <folasayosamuel@gmail.com>
2022-10-21 16:21:36 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PowerNumberWithoutMath;
+public class Program
+{
+	public static void Main(string[]args)
+	{
+		int baseNumber, expNumber, result = 1;
+		Console.Write("Base Number: ");
+		baseNumber = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Exponent Number: ");
+		expNumber = Convert.ToInt32(Console.ReadLine());
+		
+		while(expNumber != 0)
+		{
+			result *= baseNumber;
+			--expNumber;
+		}
+
+		Console.WriteLine($"Result = {result}");
+
+        Console.ReadLine();
+    }
+}
